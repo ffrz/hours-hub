f2edab59e98668d910c75f444842cf72c87c82e1
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-12-23 18:09:35 +0700

    remove test component

--- resources/js/pages/admin/dashboard/Index.vue
+++ resources/js/pages/admin/dashboard/Index.vue
@@ -22,24 +22,7 @@ const showContextMenu = () => {
           <q-card-section>
             <div class="q-mb-md text-h6">Hi, {{ $page.props.auth.user.name }}</div>
             <p>Selamat datang di dashboard {{ $config.APP_NAME }}.
-              Anda saat ini login sebagai {{ $CONSTANTS.USER_ROLES[$page.props.auth.user.role] }}</p>
-
-            <q-btn flat icon="more_vert" dense square @click="showContextMenu">
-              <q-menu v-model="showMenu" anchor="bottom left" self="top left">
-                <q-list style="min-width: 150px;">
-                  <q-item clickable v-ripple v-close-popup>
-                    <q-item-section>Option 1</q-item-section>
-                  </q-item>
-                  <q-item clickable v-ripple v-close-popup>
-                    <q-item-section>Option 2</q-item-section>
-                  </q-item>
-                  <q-item clickable v-ripple v-close-popup>
-                    <q-item-section>Option 3</q-item-section>
-                  </q-item>
-                </q-list>
-              </q-menu>
-            </q-btn>
-
+              Anda saat ini login sebagai {{ $CONSTANTS.USER_ROLES[$page.props.auth.user.role] }}.</p>
           </q-card-section>
         </q-card>
       </div>
